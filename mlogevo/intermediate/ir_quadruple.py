6556d515c64a7a0bbcbe9c98a59f13e049e80bba
UMRnInside <30196401+UMRnInside@users.noreply.github.com>
2022-09-23 13:53:09 +0800

    IR: auto-detect immediate integers and floats

--- mlogevo/intermediate/ir_quadruple.py
+++ mlogevo/intermediate/ir_quadruple.py
@@ -26,6 +26,33 @@ I2O1_INSTRUCTIONS = set([
     "lshl", "rshl"
 ])
 
+def test_parameter_type(param: str) -> str:
+    """Test a parameter if it's a "immediate_integer", "immediate_float", "variable" or "invalid" """
+    if type(param) is not str or len(param) == 0:
+        return "invalid"
+    if param[0].isalpha() or param[0] == '_':
+        return "variable"
+
+    # Test if param is base 10 or 16
+    try:
+        x = int(param, 10)
+        return "immediate_integer"
+    except ValueError:
+        pass
+    try:
+        x = int(param, 16)
+        return "immediate_integer"
+    except ValueError:
+        pass
+    try:
+        y = float(param)
+        return "immediate_float"
+    except:
+        pass
+
+    return "invalid"
+
+
 @dataclass
 class Quadruple:
     """Quadruple IR, dataclass format."""
@@ -42,10 +69,36 @@ class Quadruple:
     # src1 relop src2, jump to dest
     relop: str = ""
 
+    # Tell if src1 or src2 is immediate integer or float
+    src1_type: str = "invalid"
+    src2_type: str = "invalid"
+
     # For asm, instruction name is "asm" (w/o quotes)
     input_vars: list[str] = field(default_factory=list)
     output_vars: list[str] = field(default_factory=list)
     raw_instructions: list[str] = field(default_factory=list)
+
+    @property
+    def src1(self) -> str:
+        return self._src1
+
+    @property
+    def src2(self) -> str:
+        return self._src2
+
+    @src1.setter
+    def src1(self, value: str) -> None:
+        self._src1 = value
+        self.src1_type = test_parameter_type(self._src1)
+
+    @src2.setter
+    def src2(self, value: str) -> None:
+        self._src2 = value
+        self.src2_type = test_parameter_type(self._src2)
+
+    def __post_init__(self):
+        self.src1_type = test_parameter_type(self.src1)
+        self.src2_type = test_parameter_type(self.src2)
 
     def dump(self) -> str:
         if self.instruction == "label":
