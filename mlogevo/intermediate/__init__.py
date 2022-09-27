bf4a4a5e9c5263ca61c3141c5d3b7ba722705414
UMRnInside <30196401+UMRnInside@users.noreply.github.com>
2022-09-27 10:52:37 +0800

    mlogevo.intermediate: fix import

---
+++ mlogevo/intermediate/__init__.py
@@ -0,0 +1,4 @@
+from .ir_quadruple import Quadruple
+from .ir_quadruple import NOARG_INSTRUCTIONS, I1_INSTRUCTIONS
+from .ir_quadruple import I1O1_INSTRUCTIONS, I2O1_INSTRUCTIONS
+from .quadruple_from_text import TextQuadrupleParser
