388fa36117590f67d52c0816cfa6eb0211f0c829
UMRnInside <30196401+UMRnInside@users.noreply.github.com>
2022-10-03 19:50:38 +0800

    backend: machine (in)dependant?

--- mlogevo/backend/backend.py
+++ mlogevo/backend/backend.py
@@ -13,11 +13,13 @@ class Backend:
         return self.outputter.compile(ir_list)
 
 def make_backend(arch="mlog", target="mlog", 
-        machine_independants=None,
-        machine_dependants=None):
+        machine_independants=dict(),
+        machine_dependants=dict()):
     """make_backend(arch='mlog', target='mlog', machine_independants={}, machine_dependants={})
     """
     backend = Backend()
     if arch == "mlog" and target == "mlog":
-        backend.outputter = IRtoMlogCompiler()
+        backend.outputter = IRtoMlogCompiler(
+                strict_32bit=machine_dependants.get("strict-32bit", False))
     return backend
+
