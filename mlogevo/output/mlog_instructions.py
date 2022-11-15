301a65bba4442b3ad7778955258ff4afa86bc099
UMRnInside <30196401+UMRnInside@users.noreply.github.com>
2022-11-15 15:14:19 +0800

    output: new IR format, use decorator

--- mlogevo/output/mlog_instructions.py
+++ mlogevo/output/mlog_instructions.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from typing import List
+from typing import List, Dict, Iterable
 
 condition_ops = {
     "==": "equal",
@@ -19,29 +19,50 @@ inverted_ops = {
     ">": "<=",
 }
 
+mlog_ir_registry: Dict = {}
 
+
+def mlog_ir_impl(name: str, types: Iterable = ()):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        if len(types) == 0:
+            mlog_ir_registry[name] = func
+            return wrapper
+        for t in types:
+            mlog_ir_registry[f"{name}_{t}"] = func
+        return wrapper
+    return decorator
+
+
+@mlog_ir_impl("set", ("i32", "f64", "obj"))
 def mlog_set(src: str, dest: str) -> List[str]:
     inst = F"set {dest} {src}"
     return [inst, ]
 
 
+@mlog_ir_impl("__funcbegin")
+@mlog_ir_impl("label")
 def mlog_label(label) -> List[str]:
     """ label name w/o leading or trailing colon . """
     pseudo_inst = F"{label}:"
     return [pseudo_inst, ]
 
 
+@mlog_ir_impl("goto")
 def mlog_jump_always(label: str) -> List[str]:
     inst = F"jump {label} always 0 0"
     return [inst, ]
 
 
+@mlog_ir_impl("if")
 def mlog_jump_if(arg1, rel_op, arg2, label) -> List[str]:
     condition_op = condition_ops[rel_op]
     inst = F"jump {label} {condition_op} {arg1} {arg2}"
     return [inst, ]
 
 
+@mlog_ir_impl("ifnot")
 def mlog_jump_ifnot(arg1, rel_op, arg2, label) -> List[str]:
     inv_op = inverted_ops[rel_op]
     condition_op = condition_ops[inv_op]
@@ -49,130 +70,135 @@ def mlog_jump_ifnot(arg1, rel_op, arg2, label) -> List[str]:
     return [inst, ]
 
 
+@mlog_ir_impl("add", ("i32", "f64"))
 def mlog_add(src1, src2, dest) -> List[str]:
     inst = F"op add {dest} {src1} {src2}"
     return [inst, ]
 
 
+@mlog_ir_impl("sub", ("i32", "f64"))
 def mlog_sub(src1, src2, dest) -> List[str]:
     inst = F"op sub {dest} {src1} {src2}"
     return [inst, ]
 
 
+@mlog_ir_impl("mul", ("i32", "f64"))
 def mlog_mul(src1, src2, dest) -> List[str]:
     inst = F"op mul {dest} {src1} {src2}"
     return [inst, ]
 
 
+@mlog_ir_impl("div", ("f64", ))
 def mlog_div(src1, src2, dest) -> List[str]:
     inst = F"op div {dest} {src1} {src2}"
     return [inst, ]
 
 
+@mlog_ir_impl("div", ("i32", ))
 def mlog_idiv(src1, src2, dest) -> List[str]:
     inst = F"op idiv {dest} {src1} {src2}"
     return [inst, ]
 
 
+@mlog_ir_impl("rem", ("i32", ))
 def mlog_mod(src1, src2, dest) -> List[str]:
     inst = F"op mod {dest} {src1} {src2}"
     return [inst, ]
 
 
+@mlog_ir_impl("floor", ("f64", ))
+@mlog_ir_impl("cvtf64", ("i32", ))
 def mlog_floor(src, dest) -> List[str]:
     inst = F"op floor {dest} {src} 0"
     return [inst, ]
 
 
+@mlog_ir_impl("ceil", ("f64", ))
 def mlog_ceil(src, dest) -> List[str]:
     inst = F"op ceil {dest} {src} 0"
     return [inst, ]
 
 
+@mlog_ir_impl("minus", ("i32", "f64"))
 def mlog_minus(src, dest) -> List[str]:
     inst = F"op sub {dest} 0 {src}"
     return [inst, ]
 
 
-# TODO: strict 32-bit logical operands
-def mlog_and(src1, src2, dest, strict_32bit) -> List[str]:
-    insts = []
-    insts.append(F"op and {dest} {src1} {src2}")
-    return insts
+@mlog_ir_impl("and", ("i32", ))
+def mlog_and(src1, src2, dest) -> List[str]:
+    return [F"op and {dest} {src1} {src2}", ]
 
 
-def mlog_or(src1, src2, dest, strict_32bit) -> List[str]:
-    insts = []
-    insts.append(F"op or {dest} {src1} {src2}")
-    return insts
+@mlog_ir_impl("or", ("i32", ))
+def mlog_or(src1, src2, dest) -> List[str]:
+    return [F"op or {dest} {src1} {src2}", ]
 
 
-def mlog_xor(src1, src2, dest, strict_32bit) -> List[str]:
-    insts = [F"op xor {dest} {src1} {src2}"]
-    if strict_32bit:
-        insts.append(F"op b-and {dest} {dest} 0xFFFFFFFF")
-    return insts
+@mlog_ir_impl("xor", ("i32", ))
+def mlog_xor(src1, src2, dest) -> List[str]:
+    return [F"op xor {dest} {src1} {src2}", ]
 
 
-def mlog_shl(src1, src2, dest, strict_32bit) -> List[str]:
-    insts = [F"op shl {dest} {src1} {src2}"]
-    if strict_32bit:
-        insts.append(F"op b-and {dest} {dest} 0xFFFFFFFF")
-    return insts
+@mlog_ir_impl("lsh", ("i32", ))
+def mlog_shl(src1, src2, dest) -> List[str]:
+    return [F"op shl {dest} {src1} {src2}", ]
 
 
-def mlog_shr(src1, src2, dest, strict_32bit) -> List[str]:
-    insts = [F"op shr {dest} {src1} {src2}"]
-    if strict_32bit:
-        insts.append(F"op b-and {dest} {dest} 0xFFFFFFFF")
-    return insts
+@mlog_ir_impl("rsh", ("i32", ))
+def mlog_shr(src1, src2, dest) -> List[str]:
+    return [F"op shr {dest} {src1} {src2}", ]
 
 
-def mlog_flip(src1, dest, strict_32bit) -> List[str]:
-    insts = []
-    if strict_32bit:
-        insts.append(F"op b-and {dest} {src1} 0xFFFFFFFF")
-        insts.append(F"op xor {dest} {src1} 0xFFFFFFFF")
-    else:
-        insts.append(F"op xor {dest} {src1} 0xFFFFFFFF")
-    return insts
+@mlog_ir_impl("not", ("i32", ))
+def mlog_flip(src1, dest) -> List[str]:
+    return [F"op xor {dest} {src1} 0xFFFFFFFF", ]
 
 
+@mlog_ir_impl("lt", ("i32", "f64"))
 def mlog_less(src1, src2, dest) -> List[str]:
     return [F"op lessThan {dest} {src1} {src2}", ]
 
 
+@mlog_ir_impl("gt", ("i32", "f64"))
 def mlog_greater(src1, src2, dest) -> List[str]:
     return [F"op greaterThan {dest} {src1} {src2}", ]
 
 
+@mlog_ir_impl("lteq", ("i32", "f64"))
 def mlog_less_equal(src1, src2, dest) -> List[str]:
     return [F"op lessThanEq {dest} {src1} {src2}", ]
 
 
+@mlog_ir_impl("gteq", ("i32", "f64"))
 def mlog_greater_equal(src1, src2, dest) -> List[str]:
     return [F"op greaterThanEq {dest} {src1} {src2}", ]
 
 
+@mlog_ir_impl("eq", ("i32", "f64"))
 def mlog_equal(src1, src2, dest) -> List[str]:
     return [F"op equal {dest} {src1} {src2}", ]
 
 
+@mlog_ir_impl("ne", ("i32", "f64"))
 def mlog_not_equal(src1, src2, dest) -> List[str]:
     return [F"op notEqual {dest} {src1} {src2}", ]
 
 
+@mlog_ir_impl("noop", ("i32", "f64"))
 def mlog_noop() -> List[str]:
     return ["op xor __mlogev_nop __mlogev_nop 0", ]
 
 
+@mlog_ir_impl("__call")
 def mlog_call(function_name) -> List[str]:
-    insts = []
-    insts.append(F"op add retaddr@{function_name} @counter 1")
-    insts.append(F"jump {function_name} always 1 1")
-    return insts
+    return [
+        F"op add retaddr@{function_name} @counter 1",
+        F"jump {function_name} always 1 1"
+    ]
 
 
+@mlog_ir_impl("__funcend")
+@mlog_ir_impl("__return")
 def mlog_return(function_name) -> List[str]:
-    inst = F"set @counter retaddr@{function_name}"
-    return [inst, ]
+    return [F"set @counter retaddr@{function_name}", ]
--- mlogevo/output/mlog_output.py
+++ mlogevo/output/mlog_output.py
@@ -5,91 +5,41 @@ from ..intermediate.ir_quadruple import NOARG_INSTRUCTIONS, \
         I1_INSTRUCTIONS, I1O1_INSTRUCTIONS, \
         I2O1_INSTRUCTIONS, Quadruple
 from .abstract_ir_converter import AbstractIRConverter
-from .mlog_instructions import \
-    mlog_set, mlog_label, mlog_jump_always, \
-    mlog_jump_if, mlog_jump_ifnot, \
-    mlog_add, mlog_sub, mlog_mul, mlog_div, \
-    mlog_idiv, mlog_mod, \
-    mlog_floor, mlog_ceil, mlog_minus, \
-    mlog_and, mlog_or, mlog_xor, \
-    mlog_shl, mlog_shr, mlog_flip, \
-    mlog_less, mlog_greater, \
-    mlog_less_equal, mlog_greater_equal, \
-    mlog_equal, mlog_not_equal, \
-    mlog_call, mlog_return, \
-    mlog_noop
+from .mlog_instructions import mlog_ir_registry
+
+
+def strip_labels(mlog_list) -> List[str]:
+    labels = {}
+    results = []
+    counter = 0
+    for inst in mlog_list:
+        if inst.endswith(":"):
+            label_name = inst[:-1]
+            labels[label_name] = counter
+        elif len(inst.split()) == 0:
+            pass
+        else:
+            counter += 1
+            results.append(inst)
+    for (line_no, inst) in enumerate(results):
+        if inst.startswith("jump "):
+            tokens = inst.split()
+            tokens[1] = str( labels[tokens[1]] )
+            results[line_no] = " ".join(tokens)
+    return results
 
 
 class IRtoMlogConverter(AbstractIRConverter):
     def __init__(self, strict_32bit=False, keep_labels=False):
         self.strict_32bit = strict_32bit
         self.keep_labels = keep_labels
-        self.registry = {}
-        self.register_instructions()
-
-    def register_instructions(self):
-        wrapped_and = lambda src1, src2, dest: mlog_and(src1, src2, dest, self.strict_32bit)
-        wrapped_or = lambda src1, src2, dest: mlog_or(src1, src2, dest, self.strict_32bit)
-        wrapped_xor = lambda src1, src2, dest: mlog_xor(src1, src2, dest, self.strict_32bit)
-        wrapped_shl = lambda src1, src2, dest: mlog_shl(src1, src2, dest, self.strict_32bit)
-        wrapped_shr = lambda src1, src2, dest: mlog_shr(src1, src2, dest, self.strict_32bit)
-        wrapped_flip = lambda src1, src2, dest: mlog_flip(src1, dest, self.strict_32bit)
-        self.registry["setl"] = self.registry["fset"] = mlog_set
-        self.registry["label"] = mlog_label
-        self.registry["goto"] = mlog_jump_always
-        self.registry["addl"] = self.registry["fadd"] = mlog_add
-        self.registry["subl"] = self.registry["fsub"] = mlog_sub
-        self.registry["mull"] = self.registry["fmul"] = mlog_mul
-        self.registry["fdiv"] = mlog_div
-        self.registry["divl"] = mlog_idiv
-        self.registry["reml"] = mlog_mod
-        self.registry["ffloor"] = mlog_floor
-        self.registry["fceil"] = mlog_ceil
-        self.registry["minusl"] = self.registry["fminus"] = mlog_minus
-        self.registry["andl"] = wrapped_and
-        self.registry["orl"] = wrapped_or
-        self.registry["xorl"] = wrapped_xor
-        self.registry["notl"] = wrapped_flip
-        self.registry["lshl"] = wrapped_shl
-        self.registry["rshl"] = wrapped_shr
-        self.registry["noop"] = mlog_noop
-        self.registry["ltl"] = self.registry["flt"] = mlog_less
-        self.registry["lteql"] = self.registry["flteq"] = mlog_less_equal
-        self.registry["gtl"] = self.registry["fgt"] = mlog_greater
-        self.registry["gteql"] = self.registry["fgteq"] = mlog_greater_equal
-        self.registry["eql"] = self.registry["feq"] = mlog_equal
-        self.registry["nel"] = self.registry["fne"] = mlog_not_equal
-        self.registry["__funcbegin"] = mlog_label
-        self.registry["__funcend"] = mlog_return
-        self.registry["__call"] = mlog_call
-        self.registry["__return"] = mlog_return
-
-    def strip_labels(self, mlog_list) -> List[str]:
-        labels = {}
-        results = []
-        counter = 0
-        for inst in mlog_list:
-            if inst.endswith(":"):
-                label_name = inst[:-1]
-                labels[label_name] = counter
-            elif len(inst.split()) == 0:
-                pass
-            else:
-                counter += 1
-                results.append(inst)
-        for (line_no, inst) in enumerate(results):
-            if inst.startswith("jump "):
-                tokens = inst.split()
-                tokens[1] = str( labels[tokens[1]] )
-                results[line_no] = " ".join(tokens)
-        return results
 
     def convert(self, ir_list) -> str:
         results = []
         for quadruple in ir_list:
             results.extend(self.convert_single_quadruple(quadruple))
         if not self.keep_labels:
-            results = self.strip_labels(results)
+            results = strip_labels(results)
         return "\n".join(results)
 
     def convert_single_quadruple(self, quadruple: Quadruple) -> List[str]:
@@ -97,20 +47,25 @@ class IRtoMlogConverter(AbstractIRConverter):
         src1, src2, dest = quadruple.src1, quadruple.src2, quadruple.dest
         if instruction == "asm":
             return quadruple.raw_instructions
+        handler = mlog_ir_registry[instruction]
         if instruction in ("if", "ifnot"):
-            jump_function = mlog_jump_if if quadruple.instruction == "if" else mlog_jump_ifnot
             relop = quadruple.relop
-            return jump_function(src1, relop, src2, dest)
+            return handler(src1, relop, src2, dest)
         # TODO: raise an error if instruction is unknown
-        handler = self.registry[instruction]
         if instruction in NOARG_INSTRUCTIONS:
             return handler()
         if instruction in I1_INSTRUCTIONS:
             return handler(src1)
         if instruction in I1O1_INSTRUCTIONS:
-            return handler(src1, dest)
+            result = handler(src1, dest)
+            if self.strict_32bit:
+                result.append(f"op and {dest} {dest} 4294967295")
+            return result
         if instruction in I2O1_INSTRUCTIONS:
-            return handler(src1, src2, dest)
+            result = handler(src1, src2, dest)
+            if self.strict_32bit:
+                result.append(f"op and {dest} {dest} 4294967295")
+            return result
         raise ValueError(f"Unrecognized IR: {repr(instruction)}")
 
 
