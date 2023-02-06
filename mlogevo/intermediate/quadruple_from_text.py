a907583b3c22bbbba223e840f6ef8e6317f32841
UMRnInside <30196401+UMRnInside@users.noreply.github.com>
2023-02-06 12:15:11 +0800

    ir: fix asm parsing, add function extractor

--- mlogevo/intermediate/quadruple_from_text.py
+++ mlogevo/intermediate/quadruple_from_text.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from typing import List
+from typing import List, Iterable
 
 from .ir_quadruple import NOARG_INSTRUCTIONS, \
     O1_INSTRUCTIONS, I1_INSTRUCTIONS, I1O1_INSTRUCTIONS, \
     I2O1_INSTRUCTIONS, Quadruple
+from .function import Function
 
 
 class TextQuadrupleParser:
@@ -25,17 +26,16 @@ class TextQuadrupleParser:
 
             inst = tokens[0]
             if inst not in ("__asmend", "__asmvend") and self.inside_asm_block:
-                self.current_asm.raw_instructions.append(line)
+                self.current_asm.raw_instructions.append(line.strip())
                 continue
             if inst in ("__asmbegin", "__asmvbegin") and not self.inside_asm_block:
                 self.inside_asm_block = True
-                self.current_asm = Quadruple("asm_volatile" if inst == "__asmvbegin" else "__asmbegin")
+                self.current_asm = Quadruple("asm_volatile" if inst == "__asmvbegin" else "asm")
                 self.current_asm.input_vars = tokens[2:]
                 continue
             if inst in ("__asmend", "__asmvend"):
                 self.inside_asm_block = False
                 self.current_asm.output_vars = tokens[2:]
-                results.append(self.current_asm)
                 continue
 
             if inst.startswith(":"):
@@ -68,6 +68,35 @@ class TextQuadrupleParser:
                 continue
                 
         return results
+
+
+def extract_functions_from_ir(ir_list: Iterable[Quadruple]):
+    current_function: Function = None
+    init_irs = []
+    functions = {}
+    is_inside_function = False
+    for ir in ir_list:
+        if ir.instruction == "__funcbegin":
+            # TODO: other attributes
+            current_function = Function(
+                name=ir.src1,
+                result_type=None,
+                params=None,
+                local_vars=None,
+                instructions=[],
+                attributes=ir.dest.split(",")
+            )
+            current_function.instructions.append(ir)
+            is_inside_function = True
+        elif is_inside_function:
+            current_function.instructions.append(ir)
+            if ir.instruction == "__funcend":
+                functions[current_function.name] = current_function
+                is_inside_function = False
+                current_function = None
+        else:
+            init_irs.append(ir)
+    return init_irs, functions
 
 
 if __name__ == "__main__":
